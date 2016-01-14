bd1b20ba0e1f9a3b0bb072148556d72c167266c1
Tom Lazar <16lazart@elmbrookstudents.org>
2016-01-13 20:05:23 -0600

    It might work

---
+++ src/PneumaticsControl.h
@@ -0,0 +1,72 @@
+#ifndef PNEUMATICSCONTROL_H
+#define PNEUMATICSCONTROL_H
+
+#include "WPILib.h"
+#include "XboxController.h"
+
+#define SHOOTERBOOTTIME 2.0
+#define SHOOTERRESTPERIOD 3.0
+
+class PneumaticsControl {
+
+public:
+	~PneumaticsControl() {
+	}
+	PneumaticsControl();
+	static PneumaticsControl *getInstance();
+	void initialize();
+	//void run();
+	//bool CompressorFull();
+	//void shift();
+	void shiftUp();
+	void shiftDown();
+	bool isHighGear();
+	bool ballGrabberIsExtended();
+	void disable();
+	void ballGrabberExtend();
+	void ballGrabberRetract();
+	void compressorEnable();
+	void compressorDisable();
+	int ReadSwitch();
+	void ballGrabberToggle() {
+		if (isBallGrabberExtended) {
+			ballGrabberRetract();
+		} else {
+			ballGrabberExtend();
+		}
+	}
+
+private:
+
+	Timer autoTimer;
+	Timer solenoidTimer;
+	XboxController *xbox;
+	//DriverStationLCD *dsLCD;
+	Compressor *compressor;
+	/*
+	 //For shifting
+	 Solenoid *rightTrigger;
+	 Solenoid *leftTrigger;
+	 Solenoid *rightRetract;
+	 Solenoid *leftRetract;
+	 
+	 //For ball pick up
+	 Solenoid *extendBallGrabberR;
+	 Solenoid *extendBallGrabberL;
+	 Solenoid *retractBallGrabberR;
+	 Solenoid *retractBallGrabberL;
+	 */
+	bool shiftState;
+	bool highGear;
+	bool isBallGrabberExtended;
+
+	DoubleSolenoid *shiftControlL;
+	DoubleSolenoid *shiftControlR;
+	DoubleSolenoid *ballGrabberControlL;
+	DoubleSolenoid *ballGrabberControlR;
+	DigitalInput *ballGrabberExtendLimit;
+	
+
+
+};
+#endif 
